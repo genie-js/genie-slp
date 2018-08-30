2ce396d768721081e9b27d44248b3bbbd33370f7
Renée Kooi <renee@kooi.me>
2018-08-30 09:41:55 +0200

    array struct syntax

--- src/header.js
+++ src/header.js
@@ -1,24 +1,23 @@
-const Struct = require('awestruct')
-
-const t = Struct.types
+const struct = require('awestruct')
+const t = struct.types
 
 // SLP Header
-module.exports = Struct({
-  version: t.string(4),
-  numFrames: t.int32,
-  comment: t.string(24),
+module.exports = struct([
+  ['version', t.string(4)],
+  ['numFrames', t.int32],
+  ['comment', t.string(24)],
 
-  frames: t.array('numFrames', Struct({
-    cmdTableOffset: t.uint32,
-    outlineTableOffset: t.uint32,
-    paletteOffset: t.uint32,
-    properties: t.uint32,
+  ['frames', t.array('numFrames', struct([
+    ['cmdTableOffset', t.uint32],
+    ['outlineTableOffset', t.uint32],
+    ['paletteOffset', t.uint32],
+    ['properties', t.uint32],
 
-    width: t.int32,
-    height: t.int32,
-    hotspot: Struct({
-      x: t.int32,
-      y: t.int32
-    })
-  }))
-})
+    ['width', t.int32],
+    ['height', t.int32],
+    ['hotspot', struct([
+      ['x', t.int32],
+      ['y', t.int32]
+    ])]
+  ]))]
+])
